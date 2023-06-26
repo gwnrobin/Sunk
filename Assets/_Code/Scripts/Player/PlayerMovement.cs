15422e3773ec6a19997ca8bf6b84ebae2ccb8642
Robin <robinschellingerhout@live.nl>
2023-06-27 00:17:34 +0200

    mixer playermovement flask

--- Assets/_Code/Scripts/Player/PlayerMovement.cs
+++ Assets/_Code/Scripts/Player/PlayerMovement.cs
@@ -2,126 +2,123 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    public bool IsGrounded => _characterController.isGrounded;
+
+    [SerializeField] private float _floorDrag = .2f;
+    [SerializeField] private float _airDrag = .05f;
+
+    [SerializeField] private float _maxMovementSpeed = 2;
+    [SerializeField] private float _jumpForce = 3.5f;
+
+    [SerializeField] private bool _freeze = false;
+
     private CharacterController _characterController;
 
     private bool _isMoving = false;
     private bool _isSprinting = false;
-    private bool IsSprinting
-    {
-        set { _isSprinting = value;
 
-            if (_isSprinting)
-                _sprintMultiplier = 1.7f;
-            else
-                _sprintMultiplier = 1.0f;
-        }
-    }
-    private float _sprintMultiplier = 1f;
     private bool _isJumpPressed;
 
-    [SerializeField] private float _jumpForce = 3.5f;
+    private float _drag = .2f;
 
-    private const int GROUNDED_GRAVITY = -5;
+    private Vector2 _input = Vector2.zero;
+    private Vector3 _velocity = Vector3.zero;
+    private float _velocityY = 0;
 
-    private Vector3 _movementDirection;
-    private Vector3 _currentMovement;
+    public bool IsSprinting
+    {
+        set
+        {
+            if(_isSprinting != value)
+            {
+                _isSprinting = value;
 
-    private Vector3 _velocity;
-    public bool IsGrounded => _characterController.isGrounded;
-
-    [SerializeField] private float _deceleration = 1f;
-    [SerializeField] private float _movementSpeed = 5;
-    [SerializeField] private float _acceleration = .05f;
-
-    private Camera _playerCamera;
-
-    [SerializeField] private bool _freeze = false;
+                if (_isSprinting)
+                    _maxMovementSpeed *= 1.7f;
+                else
+                    _maxMovementSpeed /= 1.7f;
+            }
+        }
+    }
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
-        _playerCamera = Camera.main;
     }
 
     private void FixedUpdate()
     {
-        ApplyGravity();
         HandleJump();
+        HandleMovement();
+        ApplyGravity();
 
-        _currentMovement = ConvertMoveDirection();
+        _characterController.Move((_velocity + _velocityY * transform.up) * Time.fixedDeltaTime);
 
-        if (!_freeze)
+        _velocityY = _characterController.velocity.y;
+        _velocity = new Vector3(_characterController.velocity.x, 0, _characterController.velocity.z);
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 movement = transform.TransformDirection(new Vector3(_input.x, 0, _input.y));
+
+        if (_velocity.magnitude > _maxMovementSpeed)
         {
-            _characterController.Move(_currentMovement * Time.fixedDeltaTime);
-            _characterController.Move(_velocity * Time.fixedDeltaTime);
+            _velocity -= _velocity.normalized * (_velocity.magnitude - _maxMovementSpeed);
         }
-    }
 
-    /// <summary>
-    /// Applies input from jump key
-    /// </summary>
-    /// <param name="context">Value jump key</param>
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        _isJumpPressed = context.ReadValueAsButton();
-    }
-
-    /// <summary>
-    /// Apply movement from the keys
-    /// </summary>
-    /// <param name="context">Vector2 that contains input</param>
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        Vector2 input = context.ReadValue<Vector2>();
-        if (input.magnitude > 0)
-            _isMoving = true;
+        if (!IsGrounded)
+        {
+            _drag = _airDrag;
+        }
         else
-            _isMoving = false;
-
-        _movementDirection = new Vector3(input.x, 0, input.y);
-    }
-
-    /// <summary>
-    /// Apply input from sprint key
-    /// </summary>
-    /// <param name="context"></param>
-    public void OnSprint(InputAction.CallbackContext context)
-    {
-        IsSprinting = context.ReadValueAsButton();
-    }
-
-    /// <summary>
-    /// Returns movement vector based on if the player is inside the water
-    /// </summary>
-    private Vector3 ConvertMoveDirection()
-    {
-        if (_movementDirection.magnitude <= 0)
         {
-            return Mathf.Lerp(_currentMovement.magnitude, 0, _deceleration) * _currentMovement.normalized;
+            _drag = _floorDrag;
         }
-        Vector3 movement = _movementSpeed * _sprintMultiplier * (_movementDirection.x * transform.right + _movementDirection.z * transform.forward);
-        float wantedSpeed = Mathf.Lerp(_currentMovement.magnitude, movement.magnitude, _acceleration);
-        return wantedSpeed * movement.normalized;
+
+        _velocity -= _velocity * _drag;
+
+        _velocity += movement;
     }
 
     private void HandleJump()
     {
-        if (!IsGrounded || !_isJumpPressed) return;
+        if (!IsGrounded || !_isJumpPressed) 
+            return;
 
         _isJumpPressed = false;
 
-        _velocity.y = _jumpForce;
+        _velocityY = _jumpForce;
     }
 
     private void ApplyGravity()
     {
-        if (IsGrounded)
-            _velocity.y = GROUNDED_GRAVITY;
-        else
-            _velocity += Physics.gravity * Time.deltaTime;
+        _velocityY += Physics.gravity.y * Time.deltaTime;
     }
+
+    #region InputFunctions
+    public void OnJump(CallbackContext context)
+    {
+        _isJumpPressed = context.ReadValueAsButton();
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        if (_freeze)
+            return;
+
+        _input = context.ReadValue<Vector2>();
+        _isMoving = _input.magnitude > 0;
+    }
+
+    public void OnSprint(CallbackContext context)
+    {
+        IsSprinting = context.ReadValueAsButton();
+    }
+    #endregion
 }
